bb5b427c274f8408a58c09e113db03baab3109aa
tnick1502 <59287059+tnick1502@users.noreply.github.com>
2022-11-01 18:41:59 +0300

    Добавлена проверка на лимит выдачи

--- app/api/reports.py
+++ app/api/reports.py
@@ -45,6 +45,15 @@ async def create_report(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
+    count = await service.get_reports_count(user)
+
+    if count >= user.limit:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Year limit reached",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
     id = hashlib.sha1(
         f"{report_data.object_number} {report_data.laboratory_number} {report_data.test_type} {user.id}".encode("utf-8")).hexdigest()
 
