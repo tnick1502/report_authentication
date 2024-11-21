7e52affbc7a03e944a3d5a1176f2b940a20458b8
Nick <tnick1502@mail.ru>
2024-11-21 14:25:20 +0300

    fix: temporary s3 redirect

--- backend/app/api/s3.py
+++ backend/app/api/s3.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, Response, status, UploadFile, HTTPException
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, RedirectResponse
 import re
 
 from services.depends import get_s3_service
 from services.s3 import S3Service
+from config import configs
 
 router = APIRouter(
     prefix="/s3",
@@ -22,15 +23,59 @@ async def get(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Key have a wrong format"
         )
-    file = await s3_service.get(key)
-    content_type = file['ContentType']
-    body = file['Body']  # StreamingBody
 
-    # Генератор для чтения данных из потока
+    return RedirectResponse(url=f"{configs.endpoint_url}/{configs.bucket}/{key}")
+
+    '''
+    try:
+        # Получаем файл из S3
+        file = await s3_service.get(key)
+        body = file.get("Body")
+
+        # Проверяем, что body существует
+        if body is None:
+            print("File body is None for key: %s", key)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="File body is empty",
+            )
+        content_type = file.get("ContentType", "application/octet-stream")
+
+    except Exception as e:
+        print("Failed to get file from S3 for key: %s", key)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to get file from S3",
+        )
+
     async def stream_file():
-        async for chunk in body.iter_chunks(chunk_size=8192):  # Читаем порциями
-            yield chunk
-        await body.close()  # Закрываем поток после чтения
+        total_size = 0
+        try:
+            # Читаем файл по частям
+            async for chunk in body.iter_chunks(chunk_size=8192):
+                total_size += len(chunk)
+                yield chunk
+        except RuntimeError as e:
+            print("RuntimeError during streaming: %s", str(e))
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Connection to S3 was closed unexpectedly.",
+            )
+        except Exception as e:
+            print("Unexpected error during streaming: %s", str(e))
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed during file streaming",
+            )
+        finally:
+            # Закрытие body
+            if body is not None:
+                try:
+                    await body.close()
+                except Exception as close_error:
+                    print("Failed to close S3 body: %s", str(close_error))
+        print("File streaming completed. Total size: %d bytes", total_size)
 
-    # Возвращаем поток в StreamingResponse
+    # Возвращаем поток данных в StreamingResponse
     return StreamingResponse(stream_file(), media_type=content_type)
+    '''
--- backend/app/services/depends.py
+++ backend/app/services/depends.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiobotocore.session import get_session
+from botocore.config import Config
 
 from config import configs
 from db.database import async_session
@@ -39,12 +40,18 @@ async def get_statistics_service():
 # Работа с S3 через aiobotocore
 async def get_s3_service():
     session = get_session()  # Создаем сессию для работы с AWS S3
+    config = Config(
+        connect_timeout=1000,  # Таймаут соединения
+        read_timeout=1000,  # Таймаут чтения
+        retries={"max_attempts": 3, "mode": "adaptive"}  # Повтор попыток
+    )
     async with session.create_client(
             's3',
             endpoint_url=configs.endpoint_url,
             region_name=configs.region_name,
             aws_secret_access_key=configs.aws_secret_access_key,
-            aws_access_key_id=configs.aws_access_key_id
+            aws_access_key_id=configs.aws_access_key_id,
+            config=config
     ) as client:
         try:
             yield S3Service(client)
